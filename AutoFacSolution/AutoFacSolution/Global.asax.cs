86f9d12524bc9b63eb0a6803b4d2c01d2a23c390
JamesHu <513826883@qq.com>
2017-01-13 17:44:07 +0800

    修改

--- AutoFacSolution/AutoFacSolution/Global.asax.cs
+++ AutoFacSolution/AutoFacSolution/Global.asax.cs
@@ -5,6 +5,7 @@ using James.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -25,8 +26,24 @@ namespace AutoFacSolution
             builder.RegisterControllers(typeof(MvcApplication).Assembly);//注册所有的Controller
 
             #region 类操作
-            builder.RegisterType<StudentRepository>().As<IStudentRepository>().AsImplementedInterfaces();
+            //builder.RegisterType<StudentRepository>().As<IStudentRepository>().AsImplementedInterfaces();
             #endregion
+
+            #region dll操作
+            var IRepository = Assembly.Load("James.IRepository");
+            var Repository = Assembly.Load("James.Repository");
+            //根据名称约定（数据访问层的接口和实现均以Repository结尾），实现数据访问接口和数据访问实现的依赖
+            builder.RegisterAssemblyTypes(IRepository, Repository)
+              .Where(t => t.Name.EndsWith("Repository"))
+              .AsImplementedInterfaces();
+            //开发环境下，使用Stub类
+            //builder.RegisterAssemblyTypes(typeof(MvcApplication).Assembly).Where(
+            //    t => t.Name.EndsWith("Repository") && t.Name.StartsWith("Stub")).AsImplementedInterfaces();
+            //发布环境下，使用真实的数据访问层
+            //builder.RegisterAssemblyTypes(typeof(MvcApplication).Assembly).Where(
+            //   t => t.Name.EndsWith("Repository")).AsImplementedInterfaces();
+            #endregion
+
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
